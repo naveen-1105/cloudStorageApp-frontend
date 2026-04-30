fb3f98f70579dd0f6e91362d2ae9419fbe196846
naveen-1105 <naveenkushwaha629@gmail.com>
2026-04-30 09:16:05 +0000

    added npm i in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 set -e
 
+cd /home/ubuntu/cloudStorageApp-frontend
+
 git pull
+npm i
+npm run test
 npm run build
 aws s3 cp dist/ s3://cloudosphere-frontend --recursive
 aws cloudfront create-invalidation \
