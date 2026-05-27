e5510b0a84182259ba1d5367174793e4ae1d0d0f
naveen-1105 <naveenkushwaha629@gmail.com>
2026-05-27 14:26:22 +0530

    changed HOME to MY DRIVE

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -30,7 +30,7 @@ function DirectoryView() {
   console.log(dirId);
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("HOME");
+  const [directoryName, setDirectoryName] = useState("MY DRIVE");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
@@ -59,7 +59,7 @@ function DirectoryView() {
       try {
         // console.log("haa bhai ye ho rha h");
         const data = await getDirectoryItems(dirId);
-        setDirectoryName(dirId ? data.name : "HOME");
+        setDirectoryName(dirId ? data.name : "MY DRIVE");
         setDirectoriesList([...data.directories].reverse());
         setPath((prev) => {
           if (JSON.stringify(prev) === JSON.stringify(data.path)) return prev;
@@ -76,7 +76,7 @@ function DirectoryView() {
       try {
         // console.log("haa bhai ye ho rha h");
         const data = await getDirectoryItems(dirId);
-        setDirectoryName(dirId ? data.name : "HOME");
+        setDirectoryName(dirId ? data.name : "MY DRIVE");
         setDirectoriesList([...data.directories].reverse());
         setPath((prev) => {
           if (JSON.stringify(prev) === JSON.stringify(data.path)) return prev;
@@ -96,7 +96,7 @@ function DirectoryView() {
   // const loadDirectory = async () => {
   //   try {
   //     const data = await getDirectoryItems(dirId);
-  //     setDirectoryName(dirId ? data.name : "HOME");
+  //     setDirectoryName(dirId ? data.name : "MY DRIVE");
   //     setDirectoriesList([...data.directories].reverse());
   //     setPath(data.path)
   //     setFilesList([...data.files].reverse());
